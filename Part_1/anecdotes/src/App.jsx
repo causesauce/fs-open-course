fe64552341a24ebcc3c8607984fa65ad53bda7cb
adrix <adrian.chervinchuk1@gmail.com>
2025-08-28 10:11:29 -0400

    -moved anecdotes one folder closer to root folder; -implemented anecdotes task

---
+++ Part_1/anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const getRandInt = (max) => Math.floor(Math.random() * max)
+
+const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+const len = anecdotes.length;
+const maxIndex = len - 1;
+
+const App = () => {
+  const [votes, setVotes] = useState(new Array(len).fill(0));
+  const [selected, setSelected] = useState(getRandInt(maxIndex))
+  const [topVotedIndex, setTopVotedIndex] = useState(selected)
+
+  const updateSelected = () => {
+    // with collision detection
+    let newSelected = getRandInt(maxIndex)
+    while(newSelected === selected){
+      newSelected = getRandInt(maxIndex)
+    }
+    setSelected(newSelected)
+  }
+
+  const updateVotes = () => {
+    const newVotes = [...votes]
+    const updatedSelectedVote = newVotes[selected] + 1
+    newVotes[selected] = updatedSelectedVote
+
+    setVotes([...newVotes])
+
+    if (updatedSelectedVote >= newVotes[topVotedIndex]){
+      setTopVotedIndex(selected)
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>
+          {anecdotes[selected]}
+        <br/>
+          has {votes[selected]} votes
+        </p>
+        <button onClick={updateSelected}>next anecdote</button>
+        <button onClick={updateVotes}>vote</button>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>
+          {anecdotes[topVotedIndex]}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
