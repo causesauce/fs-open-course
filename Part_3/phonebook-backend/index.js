24fdb619321376ae794c204c064adc802fa08547
adrix <adrian.chervinchuk1@gmail.com>
2025-09-24 08:40:25 -0400

    - added backend static files support; - modified backend to build and copy ui code into local /dist directory; - modified api url in frontend; - added dev mode proxy for frontend

--- Part_3/phonebook-backend/index.js
+++ Part_3/phonebook-backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('jsonBody', (req, res) => {
